46ded6e906b632130fd396f3f97bd7579ca959e0
Gentle Trader <ceschiatti@gmail.com>
2011-10-14 19:43:28 -0300

    Adicionada uma splash screen

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
 
+#include <QSplashScreen>
+#include <QThread>
+
+class I : public QThread
+{
+    public:
+        static void sleep(unsigned long secs)
+        {
+            QThread::sleep(secs);
+        }
+};
+
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
+
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setPixmap(QPixmap(":imagens/splash-screen.jpg"));
+    splash->show();
+    I::sleep(3);
+
     MainWindow w;
     w.show();
+    splash->finish(&w);
+    delete splash;
 
     return a.exec();
 }
